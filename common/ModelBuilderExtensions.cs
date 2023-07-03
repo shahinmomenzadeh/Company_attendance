4e6a9c6b40893a9300c53e5536da132bb613a1ca
shahin <shahin.kurdestan@gmail.com>
2023-07-03 12:07:53 +0330

    common

---
+++ common/ModelBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using model;
+
+namespace Common.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void AddEntitiesFromAssembly(this ModelBuilder modelBuilder, Type assemblyType)
+        {
+            var entities = assemblyType.Assembly.GetExportedTypes()
+                .Where(p => !p.IsAbstract && p.IsClass && p.IsSubclassOf(typeof(IBaseEntity)) &&
+                            typeof(IBaseEntity).IsAssignableFrom(p));
+            foreach (var entity in entities)
+            {
+                modelBuilder.Entity(entity);
+            }
+        }
+    }
+}
